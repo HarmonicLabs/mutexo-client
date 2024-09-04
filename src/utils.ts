595ed5a51ad12a01a7d968e1c8ab2f0955033565
Raffa Bux <raffaelebussolotto@gmail.com>
2024-09-04 15:10:06 +0200

    utils update

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,27 @@
+import { MutexoMessage, mutexoMessageFromCborObj } from "@harmoniclabs/mutexo-messages";
+import { MutexoEventIndex } from "@harmoniclabs/mutexo-messages/dist/utils/constants";
+import { isObject } from "@harmoniclabs/obj-utils";
+import { CborArray } from "@harmoniclabs/cbor";
 
 export function eventNameToMutexoEventIndex( evtName: string ): number
 {
-  switch ( evtName ) {
-    default: throw new Error( `Unknown event name: ${evtName}` );
-  }
+    const evtIndex = MutexoEventIndex[ capitalizeFirstLetter( evtName ) ];
+
+    if( evtIndex !== undefined ) return evtIndex
+    else throw new Error( "Unknown event name: " + evtName );
+}
+
+function capitalizeFirstLetter( str: string ): string {
+    if( str.length === 0 ) throw new Error( "Invalid event string" );
+    return str.charAt( 0 ).toUpperCase() + str.slice( 1 );
+}
+
+export function parseMutexoMessage( stuff: any ): MutexoMessage
+{
+    if(!( 
+        isObject( stuff ) &&
+        stuff instanceof CborArray 
+    )) throw new Error( "Invalid message" );
+
+    return mutexoMessageFromCborObj( stuff );
 }
